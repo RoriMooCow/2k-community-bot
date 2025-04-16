3b06670ba5d1ddd43ab17154474abc7f75ff8a1f
RoriMooCow <97813330+RoriMooCow@users.noreply.github.com>
2025-04-16 15:23:29 -0400

    Update guildMemberRemove.js

--- Events/Logs/guildMemberRemove.js
+++ Events/Logs/guildMemberRemove.js
@@ -3,58 +3,66 @@ const config = require("../../config.json");
 const { getAuditLogEntry } = require("../../Functions/getAuditLogEntry.js");
 
 module.exports = {
-    name: "guildMemberRemove",
-    /**
-     * 
-     * @param {GuildMember} member 
-     */
-    async execute(member, client) {
-        const { guild, user } = member;
-        const logChannel = await guild.channels.fetch(config.logChannel);
+  name: "guildMemberRemove",
+  /**
+   *
+   * @param {GuildMember} member
+   */
+  async execute(member, client) {
+    try {
+      const { guild, user } = member;
+      const logChannel = await guild.channels.fetch(config.logChannel);
 
-        let moderator = null;
-        let reason = "None specified.";
-        let time = `<t:${Math.floor(Date.now() / 1000)}:F>`;
-        let kicked = false;
+      let moderator = null;
+      let reason = "None specified.";
+      let time = Date.now();
+      let kicked = false;
 
-        const kickLog = await getAuditLogEntry(
-            guild,
-            user.id,
-            AuditLogEvent.MemberKick
-        );
+      const kickLog = await getAuditLogEntry(
+        guild,
+        user.id,
+        AuditLogEvent.MemberKick
+      );
 
-        if (kickLog) {
-            moderator = kickLog.executor;
-            reason = kickLog.reason || reason;
-            time = `<t:${Math.floor(kickLog.createdTimestamp / 1000)}:F>`;
-            kicked = true;
-        }
+      if (kickLog) {
+        moderator = kickLog.executor;
+        reason = kickLog.reason || reason;
+        time = kickLog.createdTimestamp;
+        kicked = true;
+      }
 
-        if (!kicked) return;
+      if (!kicked) return;
 
-        const kickEmbed = new EmbedBuilder()
-            .setColor(16711680)
-            .setAuthor({
-                name: `${user.tag} has been kicked.`,
-                iconURL: user.displayAvatarURL()
-            })
-            .setFields(
-                {
-                    name: "Moderator",
-                    value: moderator
-                        ? `${moderator.tag} (<@${moderator.id}>)`
-                        : "Unknown"
-                },
-                {
-                    name: "Reason",
-                    value: reason
-                },
-                {
-                    name: "Date",
-                    value: time
-                }
-            );
-        
-        logChannel.send({ embeds: [ kickEmbed ] })
+      const kickEmbed = new EmbedBuilder()
+        .setColor(16711680)
+        .setAuthor({
+          name: `Member has been kicked.`,
+        })
+        .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+        .setFields([
+          {
+            name: "Member",
+            value: `${user.tag} (<@${user.id}>)`,
+            inline: true,
+          },
+          {
+            name: "Kicked by",
+            value: moderator
+              ? `${moderator.tag} (<@${moderator.id}>)`
+              : "Unknown",
+            inline: true,
+          },
+          {
+            name: "Reason",
+            value: reason,
+            inline: false,
+          },
+        ])
+        .setTimestamp(time);
+
+      logChannel.send({ embeds: [kickEmbed] });
+    } catch (error) {
+      console.error("Error handling guildMemberRemove:", error);
     }
-}
+  },
+};

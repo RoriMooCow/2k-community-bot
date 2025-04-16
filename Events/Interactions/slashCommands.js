5e9bbb9bb176409a24dfc936b7d0ccf85facb9ac
RoriMooCow <97813330+RoriMooCow@users.noreply.github.com>
2025-04-16 15:22:12 -0400

    Update slashCommands.js

--- Events/Interactions/slashCommands.js
+++ Events/Interactions/slashCommands.js
@@ -1,27 +1,27 @@
 const { ChatInputCommandInteraction, MessageFlags } = require("discord.js");
 
 module.exports = {
-    name: "interactionCreate",
-    /**
-     * 
-     * @param {ChatInputCommandInteraction} interaction 
-     */
-    execute(interaction, client) {
-        if(!interaction.isChatInputCommand()) return;
+  name: "interactionCreate",
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  execute(interaction, client) {
+    if (!interaction.isChatInputCommand()) return;
 
-        const command = client.commands.get(interaction.commandName);
-        if(!command) 
-            return interaction.reply({
-                content: "This command is outdated.",
-                flags: MessageFlags.Ephemeral
-        });
+    const command = client.commands.get(interaction.commandName);
+    if (!command)
+      return interaction.reply({
+        content: "This command is outdated.",
+        flags: MessageFlags.Ephemeral,
+      });
 
-        if(command.developer && interaction.user.id !== "133111159887888385")
-            return interaction.reply({
-                content: "You do not have permission to use this command. (Developer only)",
-                flags: MessageFlags.Ephemeral
-        });
+    if (command.developer && interaction.user.id !== "133111159887888385")
+      return interaction.reply({
+        content:
+          "You do not have permission to use this command. (Developer only)",
+        flags: MessageFlags.Ephemeral,
+      });
 
-        command.execute(interaction, client);
-    }
-}
+    command.execute(interaction, client);
+  },
+};
